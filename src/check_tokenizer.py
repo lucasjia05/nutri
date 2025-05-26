b68105f977e3759086d2b5ff299fac5976a502e0
archqn <lucasjia@ucsb.edu>
2025-05-26 06:22:42 +0000

    env creation script for resolving bnb cuda triton error

--- src/check_tokenizer.py
+++ src/check_tokenizer.py
@@ -1,5 +1,6 @@
 from transformers import AutoTokenizer
 
-tokenizer = AutoTokenizer.from_pretrained("/data/lucasjia/models/gemma-2-27b-it-tokenizer")
+tokenizer = AutoTokenizer.from_pretrained("/data/lucasjia/models/gemma-2-9b-it-tokenizer2",
+                                           local_files_only=True)
 
 print(tokenizer.chat_template)
