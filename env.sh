55ea4595fbb6e08a9c4320940d5797f96bc676ce
archqn <lucasjia@ucsb.edu>
2025-07-23 06:32:16 +0000

    remove some past reft attempts, begin results analysis

--- env.sh
+++
@@ -1,9 +0,0 @@
-source $HOME/miniconda3/etc/profile.d/conda.sh
-conda create -n qlora2 python=3.12 -y
-conda activate qlora2
-
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-
-pip3 install -r requirements.txt
-
-pip install --force-reinstall -v "triton==3.1.0"
--- src/scratches/check_tokenizer.py
+++
@@ -1,6 +0,0 @@
-from transformers import AutoTokenizer
-
-tokenizer = AutoTokenizer.from_pretrained("/data/lucasjia/models/gemma-7b-it-tokenizer",
-                                           local_files_only=True)
-
-print(tokenizer.chat_template)
