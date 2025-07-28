d7a8f6cbbda15066087460cd5f11618bcac80d92
archqn <lucasjia@ucsb.edu>
2025-07-28 00:53:55 +0000

    acc vs threshold/% for each prompting strategy for each nutrient

--- src/multi-nutrient/eval.py
+++ src/multi-nutrient/eval.py
@@ -454,19 +454,19 @@ def get_hash(v):
 prompt_carb = '''For the given query including a meal description, calculate the amount of carbohydrates in grams. If the serving size of any item in the
 meal is not specified, assume it is a single standard serving based on common nutritional guidelines (e.g., USDA).
 Respond with a dictionary object containing the total carbohydrates in grams as follows:
-{{"total_carbohydrates": total grams of carbohydrates for the serving}}
+{"total_carbohydrates": total grams of carbohydrates for the serving}
 For the total carbohydrates, respond with just the numeric amount of carbohydrates without extra text. If you don't know the answer,
 respond with:
-{{"total_carbohydrates": -1}}.
+{"total_carbohydrates": -1}.
 
 Query: "This morning, I had a cup of oatmeal with half a sliced banana and a glass of orange juice."
-Answer: {{"total_carbohydrates": 66.5}}
+Answer: {"total_carbohydrates": 66.5}
 
 Query: "I ate scrambled eggs made with 2 eggs and a toast for breakfast"
-Answer: {{"total_carbohydrates": 15}}
+Answer: {"total_carbohydrates": 15}
 
 Query: "Half a peanut butter and jelly sandwich."
-Answer: {{"total_carbohydrates": 25.3}}'''
+Answer: {"total_carbohydrates": 25.3}'''
 
 prompt_protein = '''For the given query including a meal description, calculate the amount of protein in grams. If the serving size of any item in the
 meal is not specified, assume it is a single standard serving based on common nutritional guidelines (e.g., USDA).
